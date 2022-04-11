c9d12983803f051130efc1e440b982d3725151ec
yudonglin506311858 <506311858@qq.com>
2022-04-11 21:11:01 +0800

    Update bulk_RNA-seq上游数据处理.sh

--- bulk_RNA-seq上游数据处理.sh
+++ bulk_RNA-seq上游数据处理.sh
@@ -39,3 +39,26 @@ bamCoverage --bam ./hisat/${i}_sorted.bam -o ./hisat/${i}_sorted.bam.bw  --binSi
 conda deactivate
 #rm ./hisat/${i}.bam;
 done
+
+
+
+
+cd /data1/yudonglin_data/yr/rawdata
+mkdir clean
+mkdir hisat
+mkdir count
+for i in {"11EB_FRAL220024682-1a","1EB_FRAL220024676-1a","4EB_FRAL220024679-1a","12EB_FRAL220024683-1a","3EB_FRAL220024678-1a","5EB_FRAL220024680-1a"};
+do 
+cutadapt --pair-filter=any --minimum-length 15 --max-n 8 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT -o ./clean/${i}_rmadp_1.fq.gz -p ./clean/${i}_rmadp_2.fq.gz ${i}_1.fq.gz ${i}_2.fq.gz >>filter.txt 2>&1
+java -jar /data/yudonglin/software/Trimmomatic-0.36/trimmomatic-0.36.jar PE -threads 40 -phred33 ./clean/${i}_rmadp_1.fq.gz ./clean/${i}_rmadp_2.fq.gz -baseout ./clean/${i}_fliter.fq.gz  AVGQUAL:20 SLIDINGWINDOW:4:15 MINLEN:15 1>>filter.txt 2>&1;
+hisat2 --threads 35 -x /data/yudonglin/reference/mm10/genome -1 ./clean/${i}_fliter_1P.fq.gz -2 ./clean/${i}_fliter_2P.fq.gz -S ./hisat/${i}.sam;
+samtools view -S ./hisat/${i}.sam -b > ./hisat/${i}.bam;
+rm ./hisat/${i}.sam;
+samtools sort ./hisat/${i}.bam -o ./hisat/${i}_sorted.bam;
+samtools index ./hisat/${i}_sorted.bam;
+featureCounts -T 30 -t exon -g gene_id -a /data/yudonglin/reference/Mus_musculus.GRCm38.102.gtf -o ./count/${i}.count ./hisat/${i}_sorted.bam >>~/count.txt 2>&1
+conda activate pyscenic
+bamCoverage --bam ./hisat/${i}_sorted.bam -o ./hisat/${i}_sorted.bam.bw  --binSize 10 -p 40
+conda deactivate
+#rm ./hisat/${i}.bam;
+done
